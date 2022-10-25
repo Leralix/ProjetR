2815b3614166cfa7ee7ebc6c141cb5959f045685
Gabe-MGNT <107551867+Gabe-MGNT@users.noreply.github.com>
2022-10-25 15:05:31 +0200

    Version améliorée

---
+++ ProjetR/app.R
@@ -0,0 +1,19 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+setwd(dirname(getwd()))
+
+
+
+
+
+source('ui.R')
+source('server.R')
+
+# Run the application 
+shinyApp(ui = ui, server = server)
