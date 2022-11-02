3d663804956c5c8bd04b4493b8f19fea39b75150
Gabe-MGNT <107551867+Gabe-MGNT@users.noreply.github.com>
2022-11-02 15:42:21 +0100

    Layout amélioré
    Layout amélioré plus ajout de pas mal de graphs.

--- ProjetR/server.R
+++ ProjetR/server.R
@@ -1,12 +1,16 @@
 library(dplyr)
 library(gapminder)
-
+library(geojsonio)
+library(broom)
+library(treemapify)
+library(tidyverse)
 
 #Transfrom CSV as Table
 df <- read.table("LaCentrale_60Pages.csv", header = TRUE, sep = ",", quote ='', dec = ".", fill = TRUE, comment.char = "")
 da=df
 da$Émissions_de_CO2=as.integer(gsub("[a-z]","",gsub("/","",gsub(" ","",da$Émissions_de_CO2))))
-
+da$prix=as.integer(parse_number(gsub(" ","",gsub("€","",da$prix))))
+da$Puissance_din=as.integer(parse_number(da$Puissance_din))
 #pie_chart=da%>%group_by(Année)%>%count(Énergie) %>% filter(Année==2020) %>% ggplot(aes(x=Année,y=n,fill=Énergie))+
 #  +      geom_bar(stat="identity",width=1)
 
@@ -19,33 +23,67 @@ spdf_fortified=spdf_fortified %>% left_join(.,da_depart,by=c("id"="Localisation"
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-  output$plot <- renderPlot({
-    
-    gapminder %>%
-      filter(year==input$years) %>%
-      ggplot(aes(x=gdpPercap, y=lifeExp,group=continent,color=continent)) +
-      geom_point(aes(size=pop))
-  })
-  
-  output$plot2 <- renderPlot({
+
+  output$Repartition_Emission <- renderPlot({
       ggplot(da,aes(x=Émissions_de_CO2))+geom_histogram()
   })
   
-  output$plot3 <- renderPlot({
+  output$NbVoiture_Per_Year <- renderPlot({
     da%>%count(Année)%>% ggplot(aes(x=Année,y=n))+geom_line()  
     })
   
-  output$plot4 <- renderPlot({
-    da%>%group_by(Année)%>%count(Énergie) %>% filter(Année==2020) %>% ggplot(aes(x=Année,y=n,fill=Énergie))+      geom_bar(stat="identity",width=1,color="white")+ coord_polar("y",start=0)+theme_void()+ggtitle("Voiture datant de 2020")
+  output$PieChart_Energie <- renderPlot({
+   #da%>%group_by(Année)%>%count(Énergie) %>% filter(Année==2020) %>% ggplot(aes(x=Année,y=n,fill=Énergie))+      geom_bar(stat="identity",width=1,color="white")+ coord_polar("y",start=0)+theme_void()+ggtitle("Voiture datant de 2020")
+    da%>%count(Énergie)%>% ggplot(aes(x="",y=n,fill=Énergie))+      geom_bar(stat="identity",width=1,color="white")+ coord_polar("y",start=0)+theme_void()+ggtitle("Type de consommation des véhicules vendus")
     })
   
-  output$plot5 <- renderPlot({
+  output$Histo_Energie <- renderPlot({
+    da%>%count(Énergie)%>% ggplot(aes(x=n,y=Énergie,fill=Énergie))+      geom_bar(stat="identity",width=1,color="white")+geom_text(aes(label=n), color="black", size=3.5)+ggtitle("Type de consommation des véhicules vendus")
+  })
+  
+  output$CritAir_Marque <- renderPlot({
     da%>%group_by(marque)%>%count(CritAir) %>% ggplot(aes(x=marque,y=n,fill=factor(CritAir)))+geom_bar(stat="identity")+scale_fill_manual(values=c("#a5fc03","green","yellow","orange","red","black"))+theme(axis.text.x=element_text(angle=90))+ggtitle("Répartition CritAir par maque")
   })
   
-  output$plot6 <- renderPlot({
+  output$Carte_NbVentes <- renderPlot({
     ggplot()+geom_polygon(data=spdf_fortified,aes(fill=n,x=long,y=lat,group=group))+theme_void()+coord_sf(xlim = c(-6,10),ylim=c(41,52))+scale_fill_viridis_b(option = "C")
     })
+  
+  output$NbVehicule_Marque <- renderPlot({
+    da%>%count(marque)%>%ggplot(aes(area=n,fill=marque,label = paste(marque, n, sep = "\n")))+geom_treemap()+geom_treemap_text(colour = "white",place = "centre",size = 15) +theme(legend.position = "none")
+  })
+  
+  output$Bubble_Emiss_Puiss_Prix <- renderPlot({
+    da%>%group_by(Nombre_de_places)%>%ggplot(aes(x=prix,y=Puissance_din,size=sqrt(Émissions_de_CO2),color=Classe_Emission))+geom_point(alpha=0.5)+xlim(input$RangeBubbleGraph[1],input$RangeBubbleGraph[2])
+  })
+  
+  
+  plotType <- function(type) {
+    switch(type,
+           CritAir= da%>%ggplot(aes(x=Année,fill=as.factor(CritAir)))+geom_bar(),
+           Contrôle_technique = da%>%ggplot(aes(x=Année,fill=as.factor(Contrôle_technique)))+geom_bar(),
+           Classe_Emission=da%>%ggplot(aes(x=Année,fill=as.factor(Classe_Emission)))+geom_bar(),
+           Norme_Euro=da%>%ggplot(aes(x=Année,fill=as.factor(Norme_Euro)))+geom_bar()
+           )
+  }
+  
+  output$Multiple_Histo <- renderPlot({
+      plotType(input$choice)
+    })
+  
+  output$table <- renderTable(head(da,5))
+
+   
+
+  co = da%>%count(Localisation)
+  co$Localisation[co$n < max(co$n)/2]="Autre"
+
+  output$Circular_Plot <- renderPlot({
+    co%>%ggplot(aes(x="",y=n,fill=Localisation))+geom_bar(stat="identity")+coord_polar("y",start=0)+theme_void()
+ 
+  })
+                                                                                                                                                                                                                                                                                                                                                                      
+
   
   
 }
--- ProjetR/ui.R
+++ ProjetR/ui.R
@@ -5,37 +5,150 @@ library(ggplot2)
 ui <- fluidPage(
   
   # Application title
-  titlePanel("Life Expectancy vs GDP per Capita"),
+  titlePanel("Les ventes de voitures d'occasion en France et l'impact écologique"),
   
-  # Sidebar with a slider input for number of bins
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput(inputId = "years",
-                  label = "Year",
-                  min = 1952,
-                  max = 2007,
-                  step = 5,
-                  value = 2007)
+  fluidRow(
+    p("Pour commencer nous pouvons analyser la réparition de nos données, avec certains graphiques"),
+      tabsetPanel(
+        tabPanel(
+          "Infos",
+            p("La table possède ",nrow(da)," lignes de données, pour ",ncol(da)," colonnes de données."),
+            p("Ce dashboard vise à observer le phénomène de la vente de véhicule d'occasion et en tirer des conclusions et en faire un lien avec son impact environnemental"),
+            p("Les données ont été scrapées depuis le site 'La Centrale Auto' de manière respectueuse.")
+          
+          
+        ),
+        tabPanel(
+          "Répartition des relevés",
+          sidebarLayout(
+            sidebarPanel(
+              p("On peut observer la répartition des véhicules vendus et voir quelle marque sont plus vendues que les autres.")
+            ),
+            mainPanel(
+              plotOutput(outputId = "NbVehicule_Marque")
+            ),
+          ),
+          
+        ),
+        tabPanel(
+          "Vente par année de mise en circulation",
+          sidebarLayout(
+            sidebarPanel(
+              p("On peut aussi observer le nombre de véhicules mis en vente selon leur année de mise en circulation."),
+              p("Et on peut constater que la tendance est un plus grand nombre de véhicule récent au dépens de ceux plus vieux."),
+              p("Cette tendance aura donc un impact sur les interprétations faites.")
+            ),
+            mainPanel(
+              plotOutput(outputId = "NbVoiture_Per_Year")
+            ),
+          ),
+          
+        )
+      )
     ),
     
-    # Show a plot of the generated distribution
+  
+  titlePanel("Emissions de CO2"),
+
+  sidebarLayout(
     mainPanel(
-      plotOutput(outputId = "plot")
-    )
+    plotOutput(outputId = "Repartition_Emission"),
+    ),
+    sidebarPanel(
+    p("On peut voir la majorité des véhicules vendus ont une émission de 120 g/km."),
+    p("Et que finalement l'éléctrique située à 0 prend un part non négligeable par rapport aux autres intervalles d'émissions")
+  
+    )),
+
+
+  
+  titlePanel("Répartition des types de consommation"),
+fluidRow(
+  splitLayout(
+    plotOutput(outputId = "PieChart_Energie"),
+    plotOutput(outputId = "Histo_Energie")
+  ),
+  p("La majorité des véhicules vendus fonctionnent principalement au Diesel et à l'Essence."),
+  p("Très peu de véhicules éléctriques sont vendus.")
+),
+
+titlePanel("Quelles sont les marques les moins responsables"),
+sidebarLayout(
+  sidebarPanel(
+    p("On peut en interpréter 2 choses : "),
+    p("Que beaucoup de personnes souhaitent se débarasser de leur Mercedes"),
+    p("Et que ce sont principalement les grandes marques dont les gens souhaitent vendre des véhicules au Crit'Air élevé"),
+    p("Particulièrement les marques deluxes importées des pays étrangers."),
+    p("Le score moyen Crit'Air semble être de 2")
   ),
   mainPanel(
-    plotOutput(outputId = "plot2")
-  ),
-  mainPanel(
-    plotOutput(outputId = "plot3")
-  ),
-  mainPanel(
-    plotOutput(outputId = "plot4")
-  ),
-  mainPanel(
-    plotOutput(outputId = "plot5")
-  ),
-  mainPanel(
-    plotOutput(outputId = "plot6")
+  plotOutput(outputId = "CritAir_Marque"),
   )
+),
+
+titlePanel("Localisation des ventes"),
+sidebarLayout(
+  mainPanel(
+  splitLayout(
+    plotOutput(outputId = "Carte_NbVentes"),
+    plotOutput(outputId = "Circular_Plot"),
+    
+  ),
+  ),
+
+  sidebarPanel(
+    p("Les régions frontalières et centrales semblent être celles où les gens vendent leur voitures."),
+    p("Mais cela reste en grande majorité dans la région parisienne, là où le véhicule est plus utilisé pour se déplacer malgré une présence transport en commun abondante."),
+    p("Le nombre de ventes totales semble même concentrée dans la région parisienne")
+    
+  )
+),
+    
+
+titlePanel("Lien entre prix, puissance et émission"),
+
+  sidebarLayout(
+    sidebarPanel(
+    sliderInput(
+      inputId = "RangeBubbleGraph",
+      label="Prix",
+      min=1000,
+      max=300000,
+      step=1,
+      value=c(5000,10000),
+      ticks=TRUE,
+  ),
+  p("Plus le prix de vente augmente, plus les véhicules semblent être puissant et consommé beaucoup et être mal classés"),
+  p("Même si on peut observer que dans les prix bas, la puissance est moindre mais la consommation nettement plus basse"),
+  p("Et même si le prix semble corrélé à l'émission du véhicule, on peut voir que le lien entre puissance et consommation et bien plus flagrant.")
+    ),
+  mainPanel(
+  plotOutput(outputId = "Bubble_Emiss_Puiss_Prix")
+  ),
+  ),
+  
+
+titlePanel("Année du véhicule et lien avec son impact écologie"),
+  sidebarLayout(
+    
+    sidebarPanel(
+      radioButtons(
+        inputId = "choice",
+        label = "Choix de visualisation",
+        c("CritAir",
+          "Contrôle_technique",
+          "Classe_Emission",
+          "Norme_Euro"
+      )
+      
+    ),
+    p("Comme on peut s'y attendre, plus les véhicules sont vieux plus leur Crit'Air est mauvais et consomme plus"),
+    p("Tous les véhicules d'avant 2017 n'ont pas passé le contrôle technique, et la plupart avant cette date ne respecte même plus les nouvelles normes européennes de consommation.")
+    ),
+    mainPanel(
+      plotOutput(outputId = "Multiple_Histo")
+    )
+    
+  ),
+
 )
